65bd87c695c8f8c556485e77c04ede17cfb22341
xep097 <egilpett@me.com>
2019-12-29 01:20:44 +0100

    Create visual.js

---
+++ scripts/visual.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+  var menu = document.getElementById("nav");
+  if (menu.style.height == '300px') {
+    menu.style.height = '0px';
+  } else {
+    menu.style.height = '300px'
+  }
+}
